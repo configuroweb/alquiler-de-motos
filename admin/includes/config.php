3f57c664485a37aba1fda4a314d0531f5c0d5cf9
Mauricio Sevilla Britto <msevillab@gmail.com>
2021-03-06 13:56:38 -0500

    Actualización versión funcional
    Actualización versión funcional

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -3,7 +3,7 @@
 define('DB_HOST','localhost');
 define('DB_USER','root');
 define('DB_PASS','');
-define('DB_NAME','bikerental');
+define('DB_NAME','motobase');
 // Establish database connection.
 try
 {
